38bb5c8386d089bbf9789744bd5bb37c42cea9e9
johnwaia <johnwaia25@gmail.com>
2025-09-05 13:46:03 +0200

    connect back + front

---
+++ Server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const https = require('https');
+const app = express();
+const port = 5000;
+
+
+//gestion des middleware
+app.use((req, res, next) => {
+  console.log(`Requête ${req.method} reçue sur ${req.path}`);
+
+  const origin = req.headers.origin;
+  const allowedOrigins = ['http://localhost:3000'];
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    console.log('Réponse rapide OPTIONS 204');
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello from the Node.js backend!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
